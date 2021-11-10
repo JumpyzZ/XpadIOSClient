d05b964728b4eac33d5b4ee5904914837ae258de
Zhu <820909823@qq.com>
2021-11-10 13:17:10 +0800

    Initial Commit

---
+++ GameControllerIOS/GameControllerIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameControllerIOSApp.swift
+//  GameControllerIOS
+//
+//  Created by zhu on 2021/11/10.
+//
+
+import SwiftUI
+
+@main
+struct GameControllerIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
